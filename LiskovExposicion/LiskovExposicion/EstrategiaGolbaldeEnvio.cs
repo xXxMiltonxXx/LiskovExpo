0bec8567e010c617864570642db4677c2493f39b
xXxMiltonxXx <93544094+xXxMiltonxXx@users.noreply.github.com>
2021-12-08 04:45:48 -0500

    Add files via upload

---
+++ LiskovExposicion/LiskovExposicion/EstrategiaGolbaldeEnvio.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace LiskovExposicion
+{
+    public class EstrategiaGolbaldeEnvio : OrganizaciondeEnvio
+    {
+        public EstrategiaGolbaldeEnvio(decimal PrecioInicial)
+            : base(PrecioInicial)
+        {
+        }
+        public new decimal precioInicial
+        {
+            get
+            {
+                return base.precioInicial;
+            }
+            set
+            {
+                base.precioInicial = value;
+            }
+        }
+        public override decimal CalcularCostodeEnvio(float PesoenKg, RegionInfo Destino)
+        {
+            //Precondicion
+            if (PesoenKg <= 0)
+                throw new ArgumentOutOfRangeException(nameof(PesoenKg), "El peso en kilogramos debe se mayor a cero");
+            if (Destino == null)
+                throw new ArgumentNullException("Debe de existir una localización");
+            //Post condicion
+            var CostodeEnvio = decimal.One;
+            if (Destino == RegionInfo.CurrentRegion)
+            
+                CostodeEnvio= decimal.Zero;
+            
+            return CostodeEnvio;
+        }
+
+    }
+
+       
+
+            
+       
+
+
+    }
---
+++ LiskovExposicion/LiskovExposicion/OrganizaciondeEnvio.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace LiskovExposicion
+{ 
+    public class OrganizaciondeEnvio
+
+    {   public OrganizaciondeEnvio(decimal PrecioInicial)
+        {
+            this.precioInicial = PrecioInicial; 
+        }
+        //Invariante
+        private decimal PrecioInicial;
+        protected decimal precioInicial
+        {
+            get
+            {
+                return this.PrecioInicial;
+            }
+            set
+            {
+                if (value<= 0)
+                    throw new ArgumentOutOfRangeException(nameof(value), "El precio inicial deb ser postivo distinto a cero");
+                this.PrecioInicial = value;
+            }
+        }
+        
+        public virtual decimal CalcularCostodeEnvio(float PesoenKg, RegionInfo Destino)
+        {   
+            //Precondicion
+            if (PesoenKg <= 0)
+                throw new ArgumentOutOfRangeException(nameof(PesoenKg), "El peso en kilogramos debe se mayor a cero");
+            //Implementación
+            var CostodeEnvio = decimal.One;
+            //Post condición
+            if (CostodeEnvio <= decimal.Zero)
+                throw new ArgumentOutOfRangeException("Resultado, el envio debe ser mayor a cero");
+            return CostodeEnvio;
+        }
+    }
+}
